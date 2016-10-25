8b7da86e55d50708fb3e9de771efc74db6640d49
Henry Qi <henryqi222@gmail.com>
2016-10-25 14:39:56 -0600

    added header and footer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-angular.module('internApp', [])
+angular.module('internApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider){
+	
+	$urlRouterProvider
+		.otherwise('/');
+	
+	$stateProvider
+		.state('initial', {
+			url: '/',
+			templateUrl: "../views/initial.html"
+		})
+		.state('landing', {
+			url: '/landing',
+			templateUrl: "../views/landing.html"
+		})
+		.state('friends', {
+			url: '/friends',
+			templateUrl: "../views/friends.html"
+		})
+	  .state('search', {
+			url: '/search',
+			templateUrl: "../views/search.html"
+		})
+	  .state('profile', {
+			url: '/profile',
+			templateUrl: "../views/profile.html"
+		})
+	  .state('update', {
+			url: '/update',
+			templateUrl: "../views/update.html"
+		});
+	  
+	
+})
